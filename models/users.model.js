bd7769e3479219a75084048328b174934d25eb2a
Viktor <korneevv@gmail.com>
2024-02-04 19:41:11 +0200

    server_front_done

--- models/users.model.js
+++ models/users.model.js
@@ -1,13 +1,16 @@
 import { db } from "../config/db.js"; 
 
 export const register = (email, password) => {
-    return db("users").insert({ email, password }, ["id", 'email', "password"]);
+    // return db("users").insert({ email, password }, ["id", 'email', "password"]);
+    return db("users").insert({ email, password });
 };
 
 export const login = (email) => {
-    return db("users").select("id", 'email', "password").where({ email });
+    // return db("users").select("id", 'email', "password").where({ email });
+    return db("users").select("email", "password").where({ email });
 };
 
 export const all = () => {
-    return db("users").select("id", 'email', "password").orderBy('id');
+    // return db("users").select("id", 'email', "password").orderBy('id');
+    return db("users").select("email", "password").orderBy('id');
 };
