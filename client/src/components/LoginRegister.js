ecf76d1b29cb9b076f3906557204f7c65e9c0c02
Viktor <korneevv@gmail.com>
2024-02-08 22:20:28 +0200

    profile_problems_crash_server

--- client/src/components/LoginRegister.js
+++ client/src/components/LoginRegister.js
@@ -14,35 +14,23 @@ const LoginRegister = ({ page }) => {
   const navigate = useNavigate();
   const { error, token } = useSelector((state) => state.auth);
 
+  useEffect(() => {
+    if (token && (page === 'Login' || page === 'Register')) {
+      navigate('/profile');
+    }
+    // Если есть ошибка, отображаем её в модальном окне
+    if (error) {
+      setModalMessage(error);
+      setIsModalOpen(true);
+    }
+  }, [token, navigate, page, error]);
+
   const loginregister = async () => {
     try {
-      const response = await dispatch(authenticateUser({ email, password, url: page.toLowerCase() })).unwrap();
-      setIsModalOpen(false);
-
-      if (page === 'Login' || page === 'Register') {
-        navigate('/profile'); // Redirect to the profile page after login or registration
-      }
-    } catch (error) {
-      let errorMessage = '';
-
-      if (page === 'Login') {
-        if (error?.message === 'email not found') {
-          errorMessage = 'This email is not registered. Please sign up.';
-        } else if (error?.message === 'incorrect password') {
-          errorMessage = 'The password you entered is incorrect. Please try again.';
-        } else {
-          errorMessage = 'Error during login.';
-        }
-      } else if (page === 'Register') {
-        if (error?.message === 'email already in use') {
-          errorMessage = 'This email is already in use. Please use a different email.';
-        } else {
-          errorMessage = 'Error during registration.';
-        }
-      }
-
-      setModalMessage(errorMessage);
-      setIsModalOpen(true);
+      await dispatch(authenticateUser({ email, password, url: page.toLowerCase() })).unwrap();
+      // Модальное окно автоматически закрывается через useEffect при успешном входе
+    } catch (dispatchError) {
+      // Обработка ошибки перенесена в useEffect
     }
   };
 
@@ -50,14 +38,6 @@ const LoginRegister = ({ page }) => {
     setIsModalOpen(false);
     setModalMessage('');
   };
-
-  // useEffect(() => {
-  //   if (token) {
-  //     if (page === 'Login' || page === 'Register') {
-  //       navigate('/profile'); // Redirect to the profile page after login or registration
-  //     }
-  //   }
-  // }, [token, navigate, page]);
 
   return (
     <div>
@@ -69,6 +49,7 @@ const LoginRegister = ({ page }) => {
           type="email"
           label="Enter your email"
           variant="outlined"
+          value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <TextField
@@ -77,6 +58,7 @@ const LoginRegister = ({ page }) => {
           type="password"
           label="Enter your password"
           variant="outlined"
+          value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <Button variant="contained" onClick={loginregister}>
@@ -90,6 +72,5 @@ const LoginRegister = ({ page }) => {
 };
 
 export default LoginRegister;
-
 
 
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -161,5 +161,167 @@ function Profile() {
 
 export default Profile;
 
+//new
 
+// import React, { useEffect, useState } from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Box, TextField, Button, Typography, CircularProgress, Card, CardContent, Tab, Tabs, Grid, Paper } from '@mui/material';
+// import { updateUserProfile, fetchUsers, fetchDogs, fetchEvents, addDog } from '../features/users/usersSlice'; // Исправлены импорты
+// import CustomModal from './CustomModal';
+// import { selectUserProfile } from '../features/users/usersSlice';
 
+// function Profile() {
+//   const dispatch = useDispatch();
+//   const { token } = useSelector((state) => state.auth);
+//   const { user, events, dogs, status, error } = useSelector(selectUserProfile);
+//   const [email, setEmail] = useState('');
+//   const [username, setUsername] = useState('');
+//   const [firstName, setFirstName] = useState('');
+//   const [lastName, setLastName] = useState('');
+//   const [dateOfBirth, setDateOfBirth] = useState('');
+//   const [dogName, setDogName] = useState('');
+//   const [dogBreed, setDogBreed] = useState('');
+//   const [dogAge, setDogAge] = useState('');
+//   const [isModalOpen, setIsModalOpen] = useState(false);
+//   const [modalMessage, setModalMessage] = useState('');
+//   const [selectedTab, setSelectedTab] = useState(0);
+
+//   const { id } = useParams();
+
+//   useEffect(() => {
+//     if (token) {
+//       dispatch(fetchEvents());
+//       dispatch(fetchUsers(id));
+//       dispatch(fetchDogs());
+//     }
+//   }, [dispatch, token, id]);
+
+//   useEffect(() => {
+//     if (user) {
+//       setEmail(user.email || '');
+//       setUsername(user.username || '');
+//       setFirstName(user.firstName || '');
+//       setLastName(user.lastName || '');
+//       setDateOfBirth(user.dateOfBirth || '');
+//     }
+//   }, [user]);
+
+//   const handleUpdateProfile = () => {
+//     if (!user || !user.user_id) {
+//       console.error('User or User ID is undefined');
+//       setModalMessage('User information is not complete.');
+//       setIsModalOpen(true);
+//       return;
+//     }
+  
+//     dispatch(updateUserProfile({
+//       userId: user.user_id,
+//       userData: { email, username, firstName, lastName, dateOfBirth },
+//     }));
+//   };
+  
+
+//   const handleAddDog = () => {
+//     dispatch(addDog({ name: dogName, breed: dogBreed, age: dogAge }));
+//     setDogName('');
+//     setDogBreed('');
+//     setDogAge('');
+//   };
+
+//   const closeModal = () => {
+//     setIsModalOpen(false);
+//     setModalMessage('');
+//   };
+
+//   const handleTabChange = (event, newValue) => {
+//     setSelectedTab(newValue);
+//   };
+
+//   return (
+//     <Box p={3}>
+//       <Typography variant="h4" gutterBottom>My Info</Typography>
+
+//       <Paper sx={{ marginBottom: 2 }}>
+//         <Tabs value={selectedTab} onChange={handleTabChange} aria-label="profile tabs" centered>
+//           <Tab label="My Info" />
+//           <Tab label="My Dogs" />
+//           <Tab label="My Events" />
+//         </Tabs>
+//       </Paper>
+
+//       <Box mt={2}>
+//         {selectedTab === 0 && (
+//           <Grid container spacing={2}>
+//             <Grid item xs={12} sm={6}>
+//               <TextField label="Email" variant="outlined" fullWidth value={email} onChange={(e) => setEmail(e.target.value)} />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <TextField label="Username" variant="outlined" fullWidth value={username} onChange={(e) => setUsername(e.target.value)} />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <TextField label="First Name" variant="outlined" fullWidth value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <TextField label="Last Name" variant="outlined" fullWidth value={lastName} onChange={(e) => setLastName(e.target.value)} />
+//             </Grid>
+//             <Grid item xs={12} sm={6}>
+//               <TextField label="Date of Birth" variant="outlined" fullWidth type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} />
+//             </Grid>
+//             <Grid item xs={12}>
+//               <Button variant="contained" color="primary" onClick={handleUpdateProfile} sx={{ mt: 2 }}>Save</Button>
+//             </Grid>
+//           </Grid>
+//         )}
+
+//         {selectedTab === 1 && (
+//           <Box>
+//             <Typography variant="h5" gutterBottom>Add New Dog</Typography>
+//             <TextField label="Name" variant="outlined" value={dogName} onChange={(e) => setDogName(e.target.value)} fullWidth margin="normal" />
+//             <TextField label="Breed" variant="outlined" value={dogBreed} onChange={(e) => setDogBreed(e.target.value)} fullWidth margin="normal" />
+//             <TextField label="Age" variant="outlined" value={dogAge} onChange={(e) => setDogAge(e.target.value)} fullWidth margin="normal" />
+//             <Button variant="contained" color="primary" onClick={handleAddDog} sx={{ mt: 2 }}>Add Dog</Button>
+
+//             <Typography variant="h5" gutterBottom sx={{ mt: 4 }}>My Dogs</Typography>
+//             <Grid container spacing={2}>
+//               {dogs.map((dog) => (
+//                 <Grid item xs={12} sm={6} md={4} key={dog.dog_id}>
+//                   <Card variant="outlined">
+//                     <CardContent>
+//                       <Typography variant="h6">{dog.name}</Typography>
+//                       <Typography>Breed: {dog.breed}</Typography>
+//                       <Typography>Age: {dog.age} years old</Typography>
+//                     </CardContent>
+//                   </Card>
+//                 </Grid>
+//               ))}
+//             </Grid>
+//           </Box>
+//         )}
+
+//         {selectedTab === 2 && (
+//           <Grid container spacing={2}>
+//             {events.map((event) => (
+//               <Grid item xs={12} sm={6} md={4} key={event.event_id}>
+//                 <Card variant="outlined">
+//                   <CardContent>
+//                     <Typography variant="h6">{event.title}</Typography>
+//                     <Typography>{event.description}</Typography>
+//                     <Typography>Date: {event.date}</Typography>
+//                   </CardContent>
+//                 </Card>
+//               </Grid>
+//             ))}
+//           </Grid>
+//         )}
+
+//         {status === 'loading' && <CircularProgress />}
+//         {error && <Typography color="error">Error: {error}</Typography>}
+
+//         <CustomModal isOpen={isModalOpen} message={modalMessage} onRequestClose={closeModal} />
+//       </Box>
+//     </Box>
+//   );
+// }
+
+// export default Profile;
